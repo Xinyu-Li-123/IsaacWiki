f3578905ddc81516f7e0a48bae7fd502716b52ec
Eiger Li <eigerli@outlook.com>
2022-07-31 19:35:47 +0800

    pull request testing

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from pprint import pprint
 response = requests.get('https://www.google.com')
 pprint(response.status_code)
 pprint(response.headers)
+
+pprint("a message from the collaborator in a new branch to test the pull request")
